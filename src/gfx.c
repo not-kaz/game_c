86e31eac3000102c8c2a86049a8c790d7f6cfa10
not-kaz <kazislav@protonmail.com>
2024-06-01 12:57:34 +0200

    Coupled display mode data into a struct.

--- src/gfx.c
+++ src/gfx.c
@@ -1,57 +1,75 @@
 #include <glad/gl.h>
 #include <SDL2/SDL.h>
 #include <stdio.h>
+#include "config.h"
 #include "gfx.h"
 
 #define MAX_NUM_DISPLAY_MODES 512
 
-static SDL_Window *window;
-static SDL_GLContext *gl_ctx;
-static SDL_DisplayMode display_modes[MAX_NUM_DISPLAY_MODES];
+static struct config gfx_config;
+const char *gfx_config_filename = "graphics.cfg";
+static struct SDL_Window *window;
+static struct SDL_GLContext *gl_ctx;
+static struct {
+	SDL_DisplayMode modes[MAX_NUM_DISPLAY_MODES];
+	int num_modes;
+	int curr_mode_idx;
+	int idx;
+} display;
 
 static void fetch_display_modes(void)
 {
-	int display_idx;
-	int num_modes;
-
-	// NOTE: Assume main screen index is 0.
-	display_idx = 0;
-	num_modes = SDL_GetNumDisplayModes(display_idx);
-	if (num_modes < 1 ) {
+	memset(&display, 0, sizeof(display));
+	display.num_modes = SDL_GetNumDisplayModes(display.idx);
+	if (display.num_modes < 1 ) {
 		fprintf(stderr, "Failed to fetch available display modes.\n");
 		exit(EXIT_FAILURE);
 	}
-	if (num_modes > MAX_NUM_DISPLAY_MODES) {
+	fprintf(stderr, "%d\n", display.num_modes);
+	if (display.num_modes > MAX_NUM_DISPLAY_MODES) {
 		fprintf(stderr, "Number of found display modes found (%d) "
 			"exceeds our limit (%d). Not all resolutions will be "
-			"available.\n", num_modes, MAX_NUM_DISPLAY_MODES);
-		num_modes = MAX_NUM_DISPLAY_MODES;
+			"available.\n", display.num_modes, MAX_NUM_DISPLAY_MODES);
+		display.num_modes = MAX_NUM_DISPLAY_MODES;
 	}
-	for (int i = 0; i < num_modes; i++) {
-		// NOTE: All modes are zero initialized at definition.
-		//	 Failed modes can be safely ignored later on.
-		SDL_GetDisplayMode(display_idx, i, &display_modes[i]);
+	fprintf(stderr, "%d\n", display.num_modes);
+	for (int i = 0; i < display.num_modes; i++) {
+		if (SDL_GetDisplayMode(display.idx, i, &display.modes[i]) < 0) {
+			fprintf(stderr, "Failed to retrieve display info.\n");
+			fprintf(stderr, "%d\n", i);
+			exit(EXIT_FAILURE);
+		}
 	}
 }
 
 void gfx_setup(void)
 {
-	int win_width;
-	int win_height;
-	unsigned int win_flag;
+	int ww;
+	int wh;
+	unsigned int wf;
 
-	fetch_display_modes();
-	for (int i = 0; i < MAX_NUM_DISPLAY_MODES; i++) {
-		if (display_modes[i].w != 0 && display_modes[i].h != 0) {
-			win_width = display_modes[i].w;
-			win_height = display_modes[i].h;
-			break;
-		}
+	if (window || gl_ctx) {
+		return;
 	}
-	// TODO: Handle loading window properties from a config file.
-	win_flag = SDL_WINDOW_OPENGL | SDL_WINDOW_FULLSCREEN;
+	fetch_display_modes();
+	config_add_entry(&gfx_config, "window_width",
+		display.modes[display.curr_mode_idx].w);
+	config_add_entry(&gfx_config, "window_height",
+		display.modes[display.curr_mode_idx].h);
+	config_add_entry(&gfx_config, "fullscreen", 1);
+	config_add_entry(&gfx_config, "vsync", 1);
+	if (!config_read_from_file(&gfx_config, "graphics.cfg")) {
+		fprintf(stderr, "Failed to load graphics config file (%s).\n",
+			gfx_config_filename);
+	}
+	ww = config_get_entry_val(&gfx_config, "window_width");
+	wh = config_get_entry_val(&gfx_config, "window_height");
+	wf = SDL_WINDOW_OPENGL;
+	if (config_get_entry_val(&gfx_config, "fullscreen")) {
+		wf |= SDL_WINDOW_FULLSCREEN;
+	}
 	window = SDL_CreateWindow("", SDL_WINDOWPOS_CENTERED,
-		SDL_WINDOWPOS_CENTERED, win_width, win_height, win_flag);
+		SDL_WINDOWPOS_CENTERED, ww, wh, wf);
 	if (!window) {
 		fprintf(stderr, "Failed to create window. %s\n",
 			SDL_GetError());
@@ -75,14 +93,15 @@ void gfx_setup(void)
 	}
 	glEnable(GL_DEPTH_TEST);
 	// TODO: We need to call glViewport() every time window resizes.
-	glViewport(0, 0, win_width, win_height);
+	glViewport(0, 0, ww, wh);
+	//config_store_to_file(&gfx_config, "graphics.cfg");
 }
 
 void gfx_shutdown(void)
 {
 	// REVIEW: Is using memset() an optimization and how likely will it
 	//	   cause bugs if NULL is not 0x0. Profile versus simple loop.
-	memset(display_modes, 0, sizeof(display_modes));
+	memset(&display, 0, sizeof(display));
 	SDL_GL_DeleteContext(gl_ctx);
 	SDL_DestroyWindow(window);
 }
