d78edd3ecdd2d70ed804026617476972b785f10a
not-kaz <kazislav@protonmail.com>
2024-06-08 19:40:46 +0200

    Removed unnecessary bool struct field.

--- include/texture.h
+++ include/texture.h
@@ -1,13 +1,11 @@
 #ifndef TEXTURE_H
 #define TEXTURE_H
-#include <stdbool.h>
 
 #define TEXTURE_NAME_MAXLEN 32
 
 struct texture {
 	char name[TEXTURE_NAME_MAXLEN];
 	unsigned int gl_id;
-	bool is_inited;
 };
 
 int texture_init(struct texture *texture, const char *name,
